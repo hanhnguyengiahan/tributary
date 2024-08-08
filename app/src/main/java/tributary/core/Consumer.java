5750f2c79b44bb77764cb3cde6688246dcea22d0
hanhnguyengiahan <hanhnguyengiahan@github.com>
2024-08-09 02:07:22 +1000

    implement synchronised methods for partition consume

--- app/src/main/java/tributary/core/Consumer.java
+++ app/src/main/java/tributary/core/Consumer.java
@@ -4,40 +4,27 @@ import java.util.HashMap;
 
 public class Consumer {
     private String id;
-    private HashMap<Partition, Integer> partitions;
+    private Topic topic;
+    private HashMap<String, Integer> partitions;
     public Consumer(String id) {
         this.id = id;
     }
     public void consumeMessage(Partition partition) {
-        int recentConsumedOffset = partitions.get(partition);
-        // if recentConsumedOffset is not out of bound
-        if (recentConsumedOffset < partition.getMessages().size()) {
-            partitions.remove(partition);
-            partitions.put(partition, recentConsumedOffset + 1);
-        }
+        partition.consumeMessage();
     }
     public void consumeMessages(Partition partition, int numMessages) {
-        int recentConsumedOffset = partitions.get(partition);
-        int i = 1;
-        while (i < numMessages) {
-            if (recentConsumedOffset + i < partition.getMessages().size()) {
-                i++;
-            }
-        }
-        if (i == 1) {
-            consumeMessage(partition);
-            return;
-        }
-        partitions.remove(partition);
-        partitions.put(partition, recentConsumedOffset + i);
+        partition.consumeMessages(numMessages);
     }
     public void parallelConsume(Partition partition) {
         // TODO
     }
     public void addPartition(Partition partition) {
-        partitions.put(partition, 0);
+        partitions.put(partition.getId(), 0);
     }
     public void clearPartitions() {
         partitions = new HashMap<>();
+    }
+    public void playback(Partition partition, int offset) {
+        partition.playback(offset);
     }
 }
--- app/src/main/java/tributary/core/Partition.java
+++ app/src/main/java/tributary/core/Partition.java
@@ -5,6 +5,7 @@ import java.util.List;
 public class Partition {
     private String id;
     private List<Message> messages;
+    private int latestConsumedOffset = 0;
 
     public Partition(String id) {
         this.id = id;
@@ -14,7 +15,30 @@ public class Partition {
         return messages;
     }
 
-    public void addMessage(Message message) {
+    public synchronized void addMessage(Message message) {
         messages.add(message);
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public synchronized void consumeMessage() {
+        // TODO: implement consume mechanism
+        latestConsumedOffset += 1;
+    }
+    public synchronized void consumeMessages(int numMessages) {
+        int numMessageConsumed = 0;
+        while (numMessageConsumed < numMessages && latestConsumedOffset < messages.size()) {
+            // TODO: implement consume mechanism
+            latestConsumedOffset += 1;
+            numMessageConsumed += 1;
+        }
+    }
+    public synchronized void playback(int offset) {
+        // playback from offset to the latest consumed message
+        for (int i = offset; i < latestConsumedOffset; i++) {
+            // TODO: implement consume mechanism
+        }
     }
 }
