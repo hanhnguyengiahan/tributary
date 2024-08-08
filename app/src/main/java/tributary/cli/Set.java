787fc20572b82a521feed44dd24f0a582264dcb4
Luong Thang Nguyen <z5402969@ad.unsw.edu.au>
2024-08-08 22:35:07 +1000

    stub fns for Set,Show and Playback

--- app/src/main/java/tributary/cli/Set.java
+++ app/src/main/java/tributary/cli/Set.java
@@ -2,10 +2,15 @@ package tributary.cli;
 
 public class Set {
     public static void processSet(String command) {
+        String[] args = command.split(" ");
+        String function = args[1];
+        if (function.equals("consumer_group_rebalancing")) {
+            setConsumerGroupRebalancingStrategy(args[2], args[3]);
+        }
 
     }
 
-    public static void setConsumerGroupRebalancingStrategy() {
+    public static void setConsumerGroupRebalancingStrategy(String consumerGroupId, String rebalancingStrat) {
 
     }
 }
